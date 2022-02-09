228cef076e90eab1822881a31fd96e331dfeeda4
bryanbr23 <bryanbr23@users.noreply.github.com>
2022-02-09 17:30:28 +0100

    Update generateics.py

--- .github/scripts/generateics.py
+++ .github/scripts/generateics.py
@@ -4,7 +4,7 @@ import os
 import json
 import uuid
 columnsToIgnore = ["Meal Planner Queue"]    # Columns to ignore in the project
-project_name = "Test"
+project_name = "Meal Planner"
 def main():
     cardsfound = False    # This is updated if columns are found and removed. Allows for message at end if still false.
     icsevents = ""
